19598fc0cf615c035e1d56489af83e734a2d5c9b
Daniil <136441101+tohiro15@users.noreply.github.com>
2024-12-26 07:32:19 +0800

    feat: добавлена визуализация будущих улучшений и улучшено отображение стоимости товаров
    - Реализована возможность для игрока заранее увидеть, какие улучшения станут доступны при следующем повышении уровня, а также условия их получения.
    - Текстовые подписи к стоимости товаров заменены на изображения хлама, что делает интерфейс более интуитивным и приятным.

--- Assets/Scripts/Managers/ShopManager/ShopItemUI.cs
+++ Assets/Scripts/Managers/ShopManager/ShopItemUI.cs
@@ -4,11 +4,19 @@ using UnityEngine.UI;
 
 public class ShopItemUI : MonoBehaviour
 {
-        public Image ButtonImage;
-        public Image LockButtonItem;
-        public TextMeshProUGUI ItemName;
-        public TextMeshProUGUI PriceText;
-        public TextMeshProUGUI Description;
-        public TextMeshProUGUI NumberPurchase;
-        public Button BuyButton;
+    [Header("Image UI")]
+    public Image ButtonImage;
+    public Image LockButtonItem;
+
+    [Header("UnlockText UI")]
+    public TextMeshProUGUI ItemName;
+    public TextMeshProUGUI PriceText;
+    public TextMeshProUGUI Description;
+    public TextMeshProUGUI NumberPurchase;
+
+    [Header("LockText UI")]
+    public TextMeshProUGUI LockText;
+
+    [Header("Button UI")]
+    public Button BuyButton;
 }
--- Assets/Scripts/Managers/ShopManager/ShopManager.cs
+++ Assets/Scripts/Managers/ShopManager/ShopManager.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using static UnityEditor.Progress;
 
 public enum UpgradeType
 {
@@ -23,6 +24,9 @@ public class ShopManager : MonoBehaviour
         public int CurrentPurchase;
         public int TotalPurchase;
         [HideInInspector] public string NumberPurchase;
+
+        [Header("Lock Settings")]
+        public string LockText;
 
         [Header("Image Settings")]
         public Sprite ButtonImage;
@@ -84,15 +88,13 @@ public class ShopManager : MonoBehaviour
             GameObject newItem = Instantiate(itemPrefab, contentPanel);
             ShopItemUI shopItemUI = newItem.GetComponent<ShopItemUI>();
 
-            shopItemUI.ButtonImage.sprite = item.ButtonImage;
-
             if (item.isOpen)
             {
                 InitializeShopItem(shopItemUI, item, newItem);
             }
             else
             {
-                LockShopItem(shopItemUI);
+                LockShopItem(shopItemUI, item);
             }
         }
     }
@@ -103,20 +105,28 @@ public class ShopManager : MonoBehaviour
         item.CurrentPurchase = PlayerPrefs.GetInt(GetUniqueKey(CURRENT_PURCHASE_KEY, item), 1);
         item.Price = PlayerPrefs.GetInt(GetUniqueKey(PRICE_KEY, item), item.Price);
 
-        shopItemUI.LockButtonItem.gameObject.SetActive(false);
+        shopItemUI.ButtonImage.sprite = item.ButtonImage;
         shopItemUI.ItemName.text = item.ItemName;
         shopItemUI.PriceText.text = item.Price.ToString();
         shopItemUI.Description.text = item.Description;
         shopItemUI.NumberPurchase.text = item.NumberPurchase;
+
+        shopItemUI.LockButtonItem.gameObject.SetActive(false);
+        shopItemUI.LockText.gameObject.SetActive(false);
 
         newItem.transform.Find("BuyButton").GetComponent<Button>().onClick.AddListener(() => BuyItem(item, newItem));
 
         UpdateShopItemUI(shopItemUI, item);
     }
 
-    private void LockShopItem(ShopItemUI shopItemUI)
+    private void LockShopItem(ShopItemUI shopItemUI, ShopItem item)
     {
+        shopItemUI.ButtonImage.sprite = item.ButtonImage;
+        shopItemUI.LockText.text = item.LockText;
+
         shopItemUI.LockButtonItem.gameObject.SetActive(true);
+        shopItemUI.LockText.gameObject.SetActive(true);
+
         shopItemUI.ItemName.gameObject.SetActive(false);
         shopItemUI.PriceText.gameObject.SetActive(false);
         shopItemUI.Description.gameObject.SetActive(false);
@@ -186,7 +196,7 @@ public class ShopManager : MonoBehaviour
         }
         else
         {
-            shopItemUI.PriceText.text = $"{item.Price}\n’À¿Ã¿";
+            shopItemUI.PriceText.text = $"{item.Price}\n";
             shopItemUI.Description.text = item.Description;
         }
     }
@@ -204,184 +214,3 @@ public class ShopManager : MonoBehaviour
         return $"{baseKey}_{item.ItemName}";
     }
 }
-
-//public class ShopManager : MonoBehaviour
-//{
-//    [System.Serializable]
-//    public class ShopItem
-//    {
-//        [Header("Base Settings")]
-//        public string ItemName;
-//        public string Description;
-//        public int Price;
-//        public int IncreasedPrice;
-//        public int CurrentPurchase;
-//        public int TotalPurchase;
-//        [HideInInspector] public string NumberPurchase;
-
-//        [Header("Image Settings")]
-//        public Sprite ButtonImage;
-
-//        [Header("Value Settings")]
-//        public int DefaultValue;
-//        public int CurrentValue;
-
-//        [Header("Item Settings")]
-//        public bool isFireSpeed = false;
-//        public bool isFireDamage = false;
-//        public bool isBulletCount = false;
-//        public bool isOpen = false;
-//    }
-
-//    [Header("Item Settings")]
-//    [SerializeField] private List<ShopItem> items;
-//    [SerializeField] private GameObject itemPrefab;
-//    [SerializeField] private Transform contentPanel;
-
-//    [Header("UI Elements")]
-//    [SerializeField] private TextMeshProUGUI _walletUGUI;
-
-//    [Header("Sound effects")]
-//    [SerializeField] private AudioSource _baseAudioSource;
-//    [SerializeField] private AudioClip _buySoundClip;
-
-//    [Header("Scriptable Objects")]
-//    [SerializeField] private PlayerData _playerData;
-//    [SerializeField] private GameData _gameData;
-
-//    private ShopItemUI shopItemUI;
-
-//    private const string CURRENT_VALUE_KEY = "CurrentValue";
-//    private const string CURRENT_PURCHASE_KEY = "CurrentPurchase";
-//    private const string PRICE_KEY = "Price";
-//    private void Start()
-//    {
-//        SetItemAvailability();
-
-//        UpdateWalletUI();
-//        PopulateShop();
-//    }
-
-//    private void SetItemAvailability()
-//    {
-//        for (int i = 0; i < _gameData.ImprovementsUnlocked; i++)
-//        {
-//            if (_gameData.ImprovementsUnlocked < items.Count) items[i].isOpen = true;
-//            else
-//            {
-//                for (int g = 0; g < items.Count; g++)
-//                {
-//                    items[g].isOpen = true;
-//                }
-//            }
-//        }
-//    }
-//    public void PopulateShop()
-//    {
-//        foreach (ShopItem item in items)
-//        {
-//            GameObject newItem = Instantiate(itemPrefab, contentPanel);
-//            shopItemUI = newItem.GetComponent<ShopItemUI>();
-
-//            shopItemUI.ButtonImage.sprite = item.ButtonImage;
-//            shopItemUI.LockButtonItem.gameObject.SetActive(false);
-
-//            if (item.isOpen == true)
-//            {
-//                item.CurrentValue = PlayerPrefs.GetInt(GetUniqueKey(CURRENT_VALUE_KEY, item), item.DefaultValue);
-//                item.CurrentPurchase = PlayerPrefs.GetInt(GetUniqueKey(CURRENT_PURCHASE_KEY, item), 1);
-//                item.Price = PlayerPrefs.GetInt(GetUniqueKey(PRICE_KEY, item), item.Price);
-//                shopItemUI.ItemName.text = item.ItemName;
-//                shopItemUI.PriceText.text = item.Price.ToString();
-//                shopItemUI.Description.text = item.Description;
-//                shopItemUI.NumberPurchase.text = item.NumberPurchase;
-//                newItem.transform.Find("BuyButton").GetComponent<Button>().onClick.AddListener(() => BuyItem(item, newItem));
-//                UpdateShopItemUI(newItem, item);
-//            }
-//            else
-//            {
-//                shopItemUI.LockButtonItem.gameObject.SetActive(true);
-//                shopItemUI.ItemName.gameObject.SetActive(false);
-//                shopItemUI.PriceText.gameObject.SetActive(false);
-//                shopItemUI.Description.gameObject.SetActive(false);
-//                shopItemUI.NumberPurchase.gameObject.SetActive(false);
-//            }
-//        }
-//    }
-//    public void BuyItem(ShopItem item, GameObject newItem)
-//    {
-//        if (_playerData.WalletAmount >= item.Price && item.CurrentPurchase < item.TotalPurchase)
-//        {
-//            if (item.isFireSpeed == true)
-//            {
-//                PlayerPrefs.SetFloat(PlayerData.FIRE_RATE_KEY, _playerData.FireRate -= 0.1f);
-//                ApplyPurchase(item, 10);
-//            }
-//            else if (item.isFireDamage == true)
-//            {
-//                PlayerPrefs.SetInt(PlayerData.FIRE_DAMAGE_KEY, _playerData.FireDamage += 1);
-//                ApplyPurchase(item, 1);
-//            }
-//            else if (item.isBulletCount == true)
-//            {
-//                PlayerPrefs.SetInt(PlayerData.BULLET_COUNT_KEY, _playerData.BulletCount += 1);
-//                ApplyPurchase(item, 1);
-//            }
-
-//            UpdateWalletUI();
-//            UpdateShopItemUI(newItem, item);
-
-//            _baseAudioSource.PlayOneShot(_buySoundClip);
-
-//            DataManager.Instance.SaveDataAfterShop();
-
-//            SavePlayerPrefs(item);
-//        }
-//    }
-//    private void ApplyPurchase(ShopItem item, int value)
-//    {
-//        _playerData.WalletAmount -= item.Price;
-//        item.CurrentValue += value;
-//        item.CurrentPurchase += 1;
-//        item.Price += item.IncreasedPrice;
-//    }
-//    private void UpdateWalletUI()
-//    {
-//        _walletUGUI.text = $"{_playerData.WalletAmount}";
-//    }
-//    private void UpdateShopItemUI(GameObject newItem, ShopItem item)
-//    {
-//        shopItemUI = newItem.GetComponent<ShopItemUI>();
-
-//        shopItemUI.PriceText.text = item.Price.ToString();
-//        shopItemUI.Description.text = item.Description;
-
-//        shopItemUI.NumberPurchase.text = $"{item.CurrentPurchase}/{item.TotalPurchase}";
-//        if (item.CurrentPurchase >= item.TotalPurchase)
-//        {
-//            shopItemUI.PriceText.text = "MAX";
-//            if (item.isFireSpeed) shopItemUI.Description.text = $"Ã¿ —»Ã¿À‹Õ¿ﬂ — Œ–Œ—“‹ ¬€—“–≈À¿\n({item.CurrentValue})";
-//            else if (item.isFireDamage) shopItemUI.Description.text = $"Ã¿ —»Ã¿À‹Õ€… ”–ŒÕ\n({item.CurrentValue})";
-//            else if (item.isBulletCount) shopItemUI.Description.text = $"Ã¿ —»Ã¿À‹ÕŒ≈  ŒÀ»◊≈—“¬Œ œ”À‹\n({item.CurrentValue})";
-//        }
-//        else
-//        {
-//            shopItemUI.PriceText.text = $"{item.Price}\n’À¿Ã¿";
-//            if (item.isFireSpeed) shopItemUI.Description.text = $"{item.Description}";
-//            else if (item.isFireDamage) shopItemUI.Description.text = $"{item.Description}";
-//            else if (item.isBulletCount) shopItemUI.Description.text = item.Description;
-//        }
-//    }
-
-//    private void SavePlayerPrefs(ShopItem item)
-//    {
-//        PlayerPrefs.SetInt(GetUniqueKey(CURRENT_VALUE_KEY, item), item.CurrentValue);
-//        PlayerPrefs.SetInt(GetUniqueKey(CURRENT_PURCHASE_KEY, item), item.CurrentPurchase);
-//        PlayerPrefs.SetInt(GetUniqueKey(PRICE_KEY, item), item.Price);
-//        PlayerPrefs.Save();
-//    }
-//    private string GetUniqueKey(string baseKey, ShopItem item)
-//    {
-//        return $"{baseKey}_{item.ItemName}";
-//    }
-//}
